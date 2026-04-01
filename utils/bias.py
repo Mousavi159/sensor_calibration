cfafa0c84efdbb8f0aaaebe5c2e571490e06b46c
Masoumeh <masoumeh.msv1379@gmail.com>
2026-04-01 15:41:11 +0200

    Update bias.py

--- utils/bias.py
+++ utils/bias.py
@@ -3,7 +3,7 @@ import numpy as np
 def add_sinusoidal_drift_noise(signal):
     t = np.arange(len(signal))
 
-    # 🔥 bounded drift (does NOT explode)
+    # bounded drift (does NOT explode)
     drift = 5 * np.sin(t / 200)
 
     noise = np.random.normal(0, 0.5, len(signal))
@@ -21,7 +21,7 @@ def add_mixed_drift_noise(signal):
 def add_linear_drift_noise(signal):
     t = np.arange(len(signal))
 
-    drift = 0.00002 * t   # 🔥 VERY SMALL slope
+    drift = 0.00002 * t   # VERY SMALL slope
     noise = np.random.normal(0, 0.5, len(signal))
 
     return signal + drift + noise

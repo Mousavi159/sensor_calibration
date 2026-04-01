7b9685c0e37117330266bf036d41d597890bc784
Masoumeh <masoumeh.msv1379@gmail.com>
2026-04-01 15:41:38 +0200

    Update bias.py

--- utils/bias.py
+++ utils/bias.py
@@ -25,13 +25,3 @@ def add_linear_drift_noise(signal):
     noise = np.random.normal(0, 0.5, len(signal))
 
     return signal + drift + noise
-
-def add_temporal_drift(signal):
-    drift = np.zeros_like(signal)
-
-    for i in range(1, len(signal)):
-        drift[i] = drift[i-1] + np.random.normal(0, 0.1)
-
-    noise = np.random.normal(0, 2, len(signal))
-
-    return signal + drift + noise
